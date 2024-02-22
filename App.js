57562b420e504528cf8a53cc42679d2cc5d677f4
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-02-22 19:11:20 +0530

    using Custom fonts

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
-import { useState } from 'react';
-import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, ImageBackground, SafeAreaView, Text } from 'react-native';
+import { useFonts } from 'expo-font';
 import { LinearGradient } from 'expo-linear-gradient';
+import * as SplashScreen from 'expo-splash-screen';
+import AppLoading from 'expo-app-loading';
+
+import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import Colors from './constants/color';
 import GameOverScreen from './screens/GameOverScreen';
@@ -9,6 +13,18 @@ import GameOverScreen from './screens/GameOverScreen';
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
+
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
 
   const pickedNumberHandler = (pickedNumber) => {
     setUserNumber(pickedNumber);
@@ -18,6 +34,12 @@ export default function App() {
   const gameOverHandler = () => {
     setGameIsOver(true);
   };
+
+  if (!fontsLoaded) {
+    return undefined;
+  } else {
+    SplashScreen.hideAsync();
+  }
 
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
 
